079b64bada61c100722adc339ebbabc8fb0dba82
Jakub Kliment <jakub.kliment@epfl.ch>
2024-05-09 01:08:23 +0200

    petits changements encore

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 
 const Event = ({ imageUrl, date, title, month, day, place, description }) => {
   return (
-    <a href="#">
+    <a href={imageUrl}>
       <div className="flex mb-8 mx-20 my-10 bg-slate-50 rounded-lg w-4/5 h-56 transition-transform duration-300 hover:-translate-y-2">
         <div className="mr-10 flex items-center justify-center w-1/2 h-full">
           <div className="relative w-full h-full rounded-l-lg overflow-hidden">
-            <Image src={imageUrl} alt={title} layout="fill" objectFit="cover" />
+            <Image src={imageUrl} alt={title} layout="fill" objectFit="cover" />{" "}
+            // objectFit="fit" ou objectFit="cover"
           </div>
         </div>
         <div className="flex-1">
